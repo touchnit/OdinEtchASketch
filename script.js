3f2b9d29c418ab76a85a327444995e59dda99ca5
touchnit <marc.abadie64@gmail.com>
2018-10-16 10:30:16 +0200

    Final commit - Etch A Sketch

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 const gridLayer = document.querySelector('#container');
-const grid = [];
+let grid = [];
 
-function gridArray (){
+
+function clearGrid(){
+   let gridLength = grid.length;
+    for (let i=0; i< gridLength; i++){
+        grid[i].classList.remove('gridHovered');
+    }
+}
+// Delete grid function
+function deleteGrid(){
+        while (gridLayer.firstChild) {
+        container.removeChild(gridLayer.firstChild)};
+        grid.length = 0;
+}
+
+function askPlayer(){
+    let playerSquare;
+    deleteGrid();
+    do {
+    playerSquare = prompt("How many squares per side would you like to have ? ");
+    }
+    while(playerSquare > 64);
+    gridArray(Number(playerSquare));
     
-for (let i=1; i< 17; i++ )
+}
+
+function gridArray(gridsize){
+
+let gridSizeLength = Number(gridsize),
+    gridTotal = gridSizeLength * gridSizeLength,
+    gridWidthCalc = 0;
+    
+    gridWidthCalc = ( 100 / gridSizeLength );
+
+    document.documentElement.style.setProperty(`--grid-width`, gridWidthCalc + "%");
+    
+for ( let i=0; i < gridTotal ; i++ )
     {
         grid[i] = document.createElement('div');
-        grid[i].classList.add('gridDiv') ;
-        grid[i].innerHTML = [i];
+        grid[i].classList.add('gridDiv');
         gridLayer.appendChild(grid[i]);
-    };}
+        grid[i].addEventListener('mouseover', function (){
+        grid[i].classList.add('gridHovered');
+        });
+    };
+    console.log(grid);
+}
 
-gridArray();
+gridArray(16);
+
